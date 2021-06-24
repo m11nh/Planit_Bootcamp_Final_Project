8925391ed826a2851cb4346a1caec8337fa12766
Duy Minh Nguyen <greatwhitepup@hotmail.com>
2021-06-24 10:28:23 +1000

    initial setup files

---
+++ final_project/src/test/java/com/airbnb/tests/BaseTest.java
@@ -0,0 +1,54 @@
+package com.airbnb.tests;
+
+import java.lang.reflect.InvocationTargetException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.concurrent.TimeUnit;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.parallel.Execution;
+import org.junit.jupiter.api.parallel.ExecutionMode;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+// import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+@Execution(ExecutionMode.CONCURRENT)
+public abstract class BaseTest {
+    
+    protected WebDriver driver;
+
+    @BeforeEach
+    public void setup() throws MalformedURLException {
+        var options = new FirefoxOptions();
+        this.driver = new FirefoxDriver(options); //config
+
+        // 2. Set implicit wait
+        this.driver.manage().timeouts().implicitlyWait(1, TimeUnit.SECONDS); //config
+
+        // 3. Maximize
+        this.driver.manage().window().maximize();
+
+        // 4. Nav home
+        this.driver.get("https://jupiter2.cloud.planittesting.com"); //config
+    }
+
+    @AfterEach
+    public void shutdown() {
+        // 5. Close browser
+        this.driver.quit();
+    }
+
+    public <T> T open(Class<T> clazz) {
+        try {
+            return clazz.getConstructor(WebDriver.class).newInstance(this.driver);
+        } catch (InstantiationException | IllegalAccessException | IllegalArgumentException | InvocationTargetException
+                | NoSuchMethodException | SecurityException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
+// foname, email, message
---
+++ final_project/src/test/java/com/airbnb/tests/TestTest.java
@@ -0,0 +1,13 @@
+package com.airbnb.tests;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+import org.junit.jupiter.api.Test;
+
+public class TestTest extends BaseTest {
+    @Test
+    public void validateCartCalculations() {
+		System.out.println("Hello World"); 
+    }
+}
